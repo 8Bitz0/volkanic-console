dbae5b30eee8b86521ad2dbfbf97ec56a83de235
Alexander Hansen <alex@bitzero.me>
2024-10-16 11:51:26 -0400

    Move instance button into separate component and adjust default window height

--- src/routes/InstanceBar.svelte
+++ src/routes/InstanceBar.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import Icon from '@iconify/svelte';
 
+  import InstanceButton from './InstanceButton.svelte';
+
   import type { AppState } from "../scripts/State";
 
   export let state: AppState;
@@ -10,14 +12,18 @@
   }
 </script>
 
-<div class="flex flex-col w-64 h-full left-0 bg-zinc-100 dark:bg-zinc-900">
+<div class="flex flex-col w-72 h-full left-0 bg-zinc-100 dark:bg-zinc-900 overflow-hidden">
   <div class="flex flex-col w-full h-full p-3">
     <div class="flex flex-col w-full h-full gap-3">
-      <div class="flex flex-row w-full h-8 rounded-md items-center p-2 gap-1 text-zinc-700 dark:text-zinc-400 hover:bg-zinc-200 dark:hover:bg-zinc-800 hover:transition-all hover:duration-150 active:bg-zinc-300 active:scale-95 dark:active:bg-zinc-700 active:transition-all active:duration-100">
-        <Icon icon="mdi:cube-outline" />
-        <p class="text-sm flex-grow">Minecraft 1.19.2</p>
-        <div class="w-1 h-1 rounded-full bg-green-400 dark:bg-green-500"></div>
-      </div>
+      <InstanceButton class="group">
+        <Icon icon="mdi:cube-outline" class="min-w-max" />
+        <p class="max-w-[80%] overflow-hidden text-sm flex-grow text-nowrap text-ellipsis">Minecraft 1.19.2</p>
+        <div class="flex-grow" />
+        <div class="relative flex flex-row items-center justify-center">
+          <div class="w-1 min-w-max h-1 rounded-full group-hover:opacity-0 group-hover:translate-x-2 bg-green-400 dark:bg-green-500 transition-all duration-100 group-hover:transition-all group-hover:duration-100" />
+          <Icon icon="mdi:arrow-right" class="absolute w-4 min-w-max h-4 opacity-0 -translate-x-2 group-hover:translate-x-0 transition-all duration-100 group-hover:opacity-100 group-hover:transition-all group-hover:duration-100" />
+        </div>
+      </InstanceButton>
     </div>
   </div>
   <div class="flex flex-row w-full h-14 min-h-14 bg-zinc-200 dark:bg-zinc-800 items-center justify-center border-t-[1px] border-zinc-200 dark:border-zinc-800 gap-[1px]">
---
+++ src/routes/InstanceButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let onClick: () => void = () => {};
+  let className: string = "";
+
+  export { className as class };
+</script>
+
+<button
+  class="flex flex-row w-full h-8 rounded-md items-center text-left p-2 gap-1 text-zinc-700 dark:text-zinc-400 hover:bg-zinc-200 dark:hover:bg-zinc-800 transition-all duration-75 hover:transition-all hover:duration-100 active:bg-zinc-300 active:scale-[97%] dark:active:bg-zinc-700 active:transition-all active:duration-75 {className}"
+  on:click={onClick}
+>
+  <slot />
+</button>
